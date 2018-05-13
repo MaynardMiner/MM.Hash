c3eef206c2d6384f68da9b686fda00cd079ee5b9
MaynardMiner <maynardvii@gmail.com>
2018-05-12 21:06:31 -0400

    fixed more mining file settings

--- Pools/zergpool.ps1
+++ Pools/zergpool.ps1
@@ -47,7 +47,7 @@ $zergpool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Se
 		"scrypt"{$Divisor *= 1000}
 		"qubit"{$Divisor *= 1000}
 		"yescrypt"{$Divisor /= 1000}
-        		
+                "quark"{$Divisor *= 1000}		
     }
 
 			
