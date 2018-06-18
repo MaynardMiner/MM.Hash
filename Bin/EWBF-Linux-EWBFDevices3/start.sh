f2c7bc839e70ceb91ba5291ff041b953a6ba42fa
MaynardMiner <Maynardviii@gmail.com>
2018-06-18 00:07:57 -0400

    removed Bin

--- Bin/EWBF-Linux-EWBFDevices3/start.sh
+++
@@ -1,1 +0,0 @@
-./miner --server zec-eu1.nanopool.org --user t1emzuNbemjqnEhEue74NL3BxsR4cA1ajfP.YOUR_WORKER_NAME/YOUR_EMAIL --pass z --port 6666
--- Bin/NVIDIA-Tpruvot-XMR-Allium-Windows-GPUDevices2/RUN-GRLC-YIIMP.cmd
+++
@@ -1,2 +0,0 @@
-ccminer-x64 -a allium -o stratum+tcp://yiimp.eu:4443 -u benchmark -p c=GRLC --cpu-priority 3 --max-log-rate=60
-pause
