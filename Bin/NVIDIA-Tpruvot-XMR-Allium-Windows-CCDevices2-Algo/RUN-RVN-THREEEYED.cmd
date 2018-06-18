dbd4df6bea99977b98c76ca9660e38a940afae4e
MaynardMiner <Maynardviii@gmail.com>
2018-06-18 00:02:24 -0400

    Removed

--- Bin/NVIDIA-Tpruvot-XMR-Allium-Windows-CCDevices2-Algo/RUN-RVN-THREEEYED.cmd
+++
@@ -1,2 +0,0 @@
-ccminer-x64 -a x16r -o stratum+tcp://pool.threeeyed.info:3333 -u RHXzkbifjndri5QDUaUXXMB4nS87wLHmPZ -p c=RVN
-pause
--- Bin/NVIDIA-Tpruvot-XMR-Allium-Windows-CCDevices3-Coin/RUN-RVN-SUPRNOVA.cmd
+++
@@ -1,2 +0,0 @@
-ccminer-x64 -a x16r -o stratum+tcp://rvn.suprnova.cc:6666 -u tpruvot.win -p x
-pause
