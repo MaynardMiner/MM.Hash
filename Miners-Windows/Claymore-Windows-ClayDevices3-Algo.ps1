faec7ce1be36a833831ce0e4134dd43355ebf86d
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-06-25 17:13:59 -0400

    Update Claymore-Windows-ClayDevices3-Algo.ps1

--- Miners-Windows/Claymore-Windows-ClayDevices3-Algo.ps1
+++ Miners-Windows/Claymore-Windows-ClayDevices3-Algo.ps1
@@ -11,9 +11,8 @@ if($GPUDevices3 -ne '')
  }
 
  $Commands = [PSCustomObject]@{
-
-    "ethash" = ''
-	"daggerhashimoto" = ''
+    "ethash" = 'esm 2'
+    "daggerhashimoto" = '-esm 3 -estale 0'
     }
 
     $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -27,7 +26,7 @@ if($GPUDevices3 -ne '')
                 Path = $Path
                 Distro =  $Distro
                 Devices = $Devices
-                Arguments = "-mport 3335 -mode 1 -esm 3 -estale 0 -allpools 1 -allcoins 1 -epool $($Pools.(Get-Algo($_)).Host):$($Pools.(Get-Algo($_)).Port) -ewal $($Pools.(Get-Algo($_)).User3) -epsw $($Pools.(Get-Algo($_)).Pass3) $($Commands.$_)"
+                Arguments = "-mport 3335 -mode 1 -allpools 1 -allcoins 1 -epool $($Pools.(Get-Algo($_)).Host):$($Pools.(Get-Algo($_)).Port) -ewal $($Pools.(Get-Algo($_)).User3) -epsw $($Pools.(Get-Algo($_)).Pass3) $($Commands.$_)"
                 HashRates = [PSCustomObject]@{(Get-Algo($_)) = $Stats."$($Name)_$(Get-Algo($_))_HashRate".Live}
                 Selected = [PSCustomObject]@{(Get-Algo($_)) = ""}
                 API = "claymore"
