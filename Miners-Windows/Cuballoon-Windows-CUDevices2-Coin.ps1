6b29aa6ee8c843ba812b8c86e7a66f1f0edb9369
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-07-05 13:02:27 -0400

    Update Cuballoon-Windows-CUDevices2-Coin.ps1

--- Miners-Windows/Cuballoon-Windows-CUDevices2-Coin.ps1
+++ Miners-Windows/Cuballoon-Windows-CUDevices2-Coin.ps1
@@ -26,7 +26,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
      Devices = $Devices
      Arguments = "-t 0 -a $($Pools.$_.Algorithm) -o stratum+tcp://$($Pools.$_.Host):$($Pools.$_.Port) -b 0.0.0.0:4070 -u $($Pools.$_.User2) -p $($Pools.$_.Pass2) $($Commands.$_)"
      HashRates = [PSCustomObject]@{$_ = $Stats."$($Name)_$($_)_HashRate".Live}
-     API = "Ccminer"
+     API = "cuballoon"
      Selected = [PSCustomObject]@{$($Pools.$_.Algorithm) = ""}
      Port = 4070
      Wrap = $false
