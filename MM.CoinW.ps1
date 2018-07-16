295a939e5a9cde350a32cb8c63149e1ff2114653
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-07-16 17:40:41 -0400

    Update MM.CoinW.ps1

--- MM.CoinW.ps1
+++ MM.CoinW.ps1
@@ -391,23 +391,19 @@ if($LastRan -ne "")
     }
     else
     {
-     $TimeoutStats = if(Test-Path "Stats")
-	 {
-      Get-ChildItemContent "Stats" | ForEach {$_.Content | Add-Member @{Name = $_.Name} -PassThru}
-	 }
-     $Timeoutstats | ForEach-Object{
-	 if($_.Live -eq 0)
-      {
-       $Removed = Join-Path "Stats" "$($_.Name).txt"
-       $Change = $($_.Name) -replace "HashRate","TIMEOUT"
-       if(Test-Path (Join-Path "Backup" "$($Change).txt"))
+    $Stats = [PSCustomObject]@{}
+    $AllStats = if(Test-Path "Stats"){Get-ChildItemContent "Stats" | ForEach {$Stats | Add-Member $_.Name $_.Content}}
+    $Allstats | ForEach-Object{
+      if($_.Live -eq 0)
        {
-        Remove-Item (Join-Path "Backup" "$($Change).txt")
+        $Removed = Join-Path "Stats" "$($_.Name).txt"
+        $Change = $($_.Name) -replace "HashRate","TIMEOUT"
+        if(Test-Path (Join-Path "Backup" "$($Change).txt"))
+        {Remove-Item (Join-Path "Backup" "$($Change).txt")}
+        Write-Host "$($_.Name) Hashrate and Timeout Notification was Removed"
+        Write-Host "Cleared Timeouts" -ForegroundColor Red
+        }
        }
-      Remove-Item $Removed
-      Write-Host "$($_.Name) Hashrate and Timeout Notification was Removed"
-     }
-	 }
        Write-Host "Cleared Timeouts" -ForegroundColor Red
        $TimeoutTimer.Restart()
        continue
