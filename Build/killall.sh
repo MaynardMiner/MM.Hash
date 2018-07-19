2980508c383ee497633f40f8b987e1e91f4ae506
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-07-19 11:55:18 -0400

    Update killall.sh

--- Build/killall.sh
+++ Build/killall.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "Checking And Stopping All Previous Screen Sessions"
 for session in $(screen -ls | grep -o '[0-9]*\.$(< /hive/custom/MM.Hash/Build/name.sh)'); do screen -S "${session}" -X quit; done
