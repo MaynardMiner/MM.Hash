d62a1a940a31e28d4475398fb49014726fb63504
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-08-08 12:42:59 -0400

    Update MM.Hive.ps1

--- MM.Hive.ps1
+++ MM.Hive.ps1
@@ -1479,7 +1479,7 @@ Start-Sleep -s 10
            {
            if($_.WasBenchmarked -eq $False)
             {
-             if(-not Test-Path "Backup){New-Item -Path "Backup" -ItemType "Directory"}
+             if(-not (Test-Path "Backup")){New-Item -Path "Backup" -ItemType "Directory"}             
              Write-Host "$($_.Name) $($_.Coins) Starting Bench"
 	     $HashRateFilePath = Join-Path ".\Stats" "$($_.Name)_$($_.Coins)_HashRate.txt"
              $NewHashrateFilePath = Join-Path ".\Backup" "$($_.Name)_$($_.Coins)_HashRate.txt"
