2bd3f6c4dd1eae3c0d659206ed22f0a2c5b6c9bc
MaynardMiner <Maynardviii@gmail.com>
2018-08-25 16:17:07 -0400

    removed test

--- test.ps1
+++
@@ -1,6 +0,0 @@
-$ActiveMinerPrograms | foreach {
-if(($BestMiners_Combo | Where Name -EQ $_.Name | Where Path -EQ $_.Path | Where Arguments -EQ $_.Arguments | Where Type -EQ $_.Type).count -gt 0)
-{
- Write-Host "$_"
-}
-}
